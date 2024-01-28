48102a2494d82489babeeb33c75d320922628638
poopheadmaster69 <kientrung12321@gmail.com>
2024-01-28 12:05:36 +0700

    new

--- js/login.js
+++ js/login.js
@@ -1,31 +1,31 @@
-const emailElm = document.getElementById('email');
-const passwordElm = document.getElementById('password');
-const submitBtn = document.getElementById('submit-btn');
+const emailElm = document.getElementById("email");
+const passwordElm = document.getElementById("password");
+const submitBtn = document.getElementById("submit-btn");
 
 function handleLogin() {
-  if(emailElm.value === "") {
+  if (emailElm.value === "") {
     alert("Ban chua nhap email!");
     return;
   }
-  if(passwordElm.value === "") {
+  if (passwordElm.value === "") {
     alert("Ban chua nhap password!");
     return;
   }
-  const email = emailElm.value
-  const password = passwordElm.value
-  // Lay user ra tu localStorage va parse ve dang array
-  const usersLocal = localStorage.getItem('users');
+  const email = emailElm.value;
+  const password = passwordElm.value;
+
+  const usersLocal = localStorage.getItem("users");
   const users = JSON.parse(usersLocal);
-  // Kiem tra thong tin dang nhap
+
   for (let index = 0; index < users.length; index++) {
     const element = users[index];
-    if(element.email === email && element.password === password) {
+    if (element.email === email && element.password === password) {
       alert("Dang nhap thanh cong!");
-      window.location = './index.html'
+      window.location = "./index.html";
       return;
     }
   }
   alert("Sai thong tin dang nhap!");
 }
 
-submitBtn.addEventListener('click', handleLogin);
+submitBtn.addEventListener("click", handleLogin);
